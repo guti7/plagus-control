cc83a0f011af31ba579b437be6e7a9f82a8b8aca
José Gutiérrez <7505855+guti7@users.noreply.github.com>
2023-10-10 23:02:17 -0400

    Initial commit

---
+++ PlagusControl/GameScene.swift
@@ -0,0 +1,11 @@
+//
+//  GameScene.swift
+//  PlagusControl
+//
+//  Created by peche on 10/10/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+}
---
+++ PlagusControl/GameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  PlagusControl
+//
+//  Created by peche on 10/10/23.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
